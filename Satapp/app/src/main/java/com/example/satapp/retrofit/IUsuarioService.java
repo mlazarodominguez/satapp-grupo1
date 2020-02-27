5c1f57d3961a4237e3aea8a1075c85ed7bb37157
Víctor Miranda <victor.miranda.alcantara12@gmail.com>
2020-02-27 14:30:29 +0100

    perfil realizado

--- Satapp/app/src/main/java/com/example/satapp/retrofit/IUsuarioService.java
+++ Satapp/app/src/main/java/com/example/satapp/retrofit/IUsuarioService.java
@@ -1,10 +1,12 @@
 package com.example.satapp.retrofit;
 
 import com.example.satapp.models.LoginReponse;
+import com.example.satapp.models.User;
 import com.example.satapp.models.UserLogin;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
@@ -13,4 +15,8 @@ public interface IUsuarioService {
 
     @POST("auth")
     Call<LoginReponse> login(@Header("Authorization")String authHeader, @Query("access_token")String masterKey);
+
+    @GET("users/me")
+    Call<User> profile(@Query("access_token")String token);
+
 }
