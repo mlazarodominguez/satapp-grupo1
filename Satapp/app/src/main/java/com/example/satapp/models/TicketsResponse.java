27f24e1ef8d9c382e0665d252a74871d83f5b385
jmterrero <josetg999@gmail.com>
2020-03-03 13:56:36 +0100

    pull master

---
+++ Satapp/app/src/main/java/com/example/satapp/models/TicketsResponse.java
@@ -0,0 +1,11 @@
+package com.example.satapp.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketsResponse {
+}
