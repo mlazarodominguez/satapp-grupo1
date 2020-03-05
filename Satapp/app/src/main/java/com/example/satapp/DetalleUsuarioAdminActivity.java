72ad0c8d491190ad5a2144194c5d22e1269c2caa
Miguel Lázaro Domínguez <mlazaro@AULAS.TRIANA>
2020-03-05 08:42:42 +0100

    dada

---
+++ Satapp/app/src/main/java/com/example/satapp/DetalleUsuarioAdminActivity.java
@@ -0,0 +1,14 @@
+package com.example.satapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleUsuarioAdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.);
+    }
+}
